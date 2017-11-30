5f5052e1fe7032824468c7b721c09293c74b3a54
tepalex <30868978+tepalex@users.noreply.github.com>
2017-11-30 11:06:00 +0100

    Added area functions

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-	System.out.println("Hello World!");
 
-	}
-}
+	  hello("world");
+	  hello("user");
+	  hello("alex");
+
+	  double l = 5;
+    System.out.println("Square area with the length of the side " + l +" is equal " + area (l));
+
+    double a = 5;
+    double b = 6;
+
+    System.out.println("Rectangle area with the height " + a + " and the width " + b + " is equal " + area ( a, b ));
+  }
+
+	  public static void hello(String somebody) {
+      System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double s){
+      return s * s;
+  }
+
+  public static double area(double h, double w){
+      return h*w;
+  }
+  }
