dd455b1b73af1b97c0a6ba7f1cdc6084483cc32f
tepalex <30868978+tepalex@users.noreply.github.com>
2017-12-02 00:19:35 +0100

    Homework 2 v. 2.1.

--- Sandbox/src/main/java/ru/stqa/pft/homework2v2/homework2v2Part2/CalculateDistance.java
+++ Sandbox/src/main/java/ru/stqa/pft/homework2v2/homework2v2Part2/CalculateDistance.java
@@ -10,10 +10,9 @@ public class CalculateDistance {
     this.y = y;
   }
 
-  public double distance(CalculateDistance p1, CalculateDistance p2) {
-    double distance = Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  public double distance(CalculateDistance p1) {
+    double distance = Math.sqrt((this.x - p1.x) * (this.x - p1.x) + (this.y - p1.y) * (this.y - p1.y));
     return distance;
   }
-
 }
 
--- Sandbox/src/main/java/ru/stqa/pft/homework2v2/homework2v2Part2/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/homework2v2/homework2v2Part2/Point.java
@@ -6,7 +6,7 @@ public class Point {
 
     CalculateDistance p1 = new CalculateDistance(0.0, 0.0);
     CalculateDistance p2 = new CalculateDistance(3.0, 4.0);
-    System.out.println("The distance between points P1" + "(" + p1.x + "," + p1.y + ") and P2" + "(" + p2.x + "," + p2.y + ") is " + p1.distance(p1,p2));
+    System.out.println("The distance between points P1" + "(" + p1.x + "," + p1.y + ") and P2" + "(" + p2.x + "," + p2.y + ") is " + p1.distance(p2));
 
   }
 }
