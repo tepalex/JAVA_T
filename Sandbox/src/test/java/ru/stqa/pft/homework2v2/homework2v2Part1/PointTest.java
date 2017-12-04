e74c732770c27a3d8af092088dbb681959537847
tepalex <30868978+tepalex@users.noreply.github.com>
2017-12-04 12:08:26 +0100

    Added various points

--- Sandbox/src/test/java/ru/stqa/pft/homework2v2/homework2v2Part1/PointTest.java
+++ Sandbox/src/test/java/ru/stqa/pft/homework2v2/homework2v2Part1/PointTest.java
@@ -11,4 +11,20 @@ public class PointTest {
 
     Assert.assertEquals(Point.distance(p1, p2), 5.0);
   }
+
+  @Test
+  public void testDistance1 (){
+    Point p1 = new Point (1.0, 1.0);
+    Point p2 = new Point(4.0,5.0);
+
+    Assert.assertEquals(Point.distance(p1, p2), 5.0);
+  }
+
+  @Test
+  public void testDistance2 (){
+    Point p1 = new Point (-1.0, -1.0);
+    Point p2 = new Point(2.0,3.0);
+
+    Assert.assertEquals(Point.distance(p1, p2), 5.0);
+  }
 }
