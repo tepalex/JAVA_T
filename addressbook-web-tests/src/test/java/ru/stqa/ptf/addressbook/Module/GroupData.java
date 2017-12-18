0bed7cc8491f88228ac8ad4499ae26c8553a1b32
tepalex <30868978+tepalex@users.noreply.github.com>
2017-12-18 23:48:48 +0100

    Revert "Lesson 4.9"
    This reverts commit f0bb9ef2342d099c5a8f40fdae886a64db4998ed.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/Module/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/Module/GroupData.java
@@ -8,12 +8,9 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-
-
   public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
+    this.id = 0;
     this.name = name;
-
     this.header = header;
     this.footer = footer;
   }
@@ -45,6 +42,24 @@ public class GroupData {
   }
 
   @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  @Override
   public String toString() {
     return "GroupData{" +
             "id='" + id + '\'' +
@@ -52,18 +67,4 @@ public class GroupData {
             '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    return name != null ? name.hashCode() : 0;
-  }
 }
