896bce6275dabe77679af9dc7cb942a8a4243b91
tepalex <30868978+tepalex@users.noreply.github.com>
2017-12-24 08:08:29 +0100

    Lesson 4.6

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/DeleteContactTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/DeleteContactTest.java
@@ -23,5 +23,9 @@ public class DeleteContactTest extends TestBase {
     app.getContactHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size()-1);
+
+    before.remove(before.size()-1);
+    Assert.assertEquals(before, after);
+
   }
 }
