d7d55fc0ec7e454f264c1363e5d173d677da3f55
tepalex <30868978+tepalex@users.noreply.github.com>
2018-01-15 17:09:43 +0100

    Lesson 6.1

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/BaseHelper.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+
 public class BaseHelper {
   protected WebDriver wd;
 
@@ -26,6 +28,12 @@ public class BaseHelper {
         wd.findElement(locator).sendKeys(text);
       }
     }
+  }
+
+  protected void attach(By locator, File file) {
+    if (file != null){
+        wd.findElement(locator).sendKeys(file.getAbsolutePath());
+     }
   }
 
   public boolean isAlertPresent() {
