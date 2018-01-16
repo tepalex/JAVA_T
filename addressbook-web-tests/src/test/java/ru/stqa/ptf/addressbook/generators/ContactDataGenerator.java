3f64d1809d351d890051fbbba06202c34f34588c
tepalex <30868978+tepalex@users.noreply.github.com>
2018-01-16 11:41:54 +0100

    Lesson 6.3

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.ptf.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.ptf.addressbook.Module.ContactData;
 import ru.stqa.ptf.addressbook.Module.GroupData;
 
@@ -12,15 +15,30 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
+  @Parameter (names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
   public static void main (String[] args) throws IOException {
-      int count = Integer.parseInt(args[0]);
-      File file = new File(args[1]);
-
-      List<ContactData> contacts = generateContacts (count);
-      save(contacts, file);
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex){
+      jCommander.usage();
+      return;
     }
+    generator.run();
+  }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts (count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     System.out.println(new File(".").getAbsolutePath());
     for (ContactData contact : contacts){
@@ -31,7 +49,7 @@ public class ContactDataGenerator {
     writer.close();
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData()
