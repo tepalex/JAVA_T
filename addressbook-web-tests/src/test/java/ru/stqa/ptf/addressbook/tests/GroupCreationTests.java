5fe03d1a458b35091eb00dab2932ded69321f595
tepalex <30868978+tepalex@users.noreply.github.com>
2018-01-17 22:47:17 +0100

    Lesson 6.11

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.ptf.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.Module.GroupData;
@@ -18,6 +20,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
+
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
@@ -1,12 +1,21 @@
 package ru.stqa.ptf.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.ptf.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 
   protected static final ApplicationManager app
@@ -17,9 +26,18 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
       app.stop();
   }
 
+  @BeforeMethod
+  public void lodTestStart(Method m, Object[] p){
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("Stop test " + m.getName());
+}
 }
