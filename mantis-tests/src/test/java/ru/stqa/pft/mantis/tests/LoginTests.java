e095bae7124d78f0325992bea47219541a40b2ab
tepalex <30868978+tepalex@users.noreply.github.com>
2018-01-23 15:47:21 +0100

    Lesson 8.4

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -4,12 +4,9 @@ import ru.stqa.pft.mantis.appmanager.HttpSession;
 import java.io.IOException;
 import static org.testng.Assert.assertTrue;
 
-
-
 public class LoginTests extends TestBase {
 
   @Test
-
   public void testLogin() throws IOException {
     HttpSession session = app.newSession();
     assertTrue(session.login("administrator", "standard"));
